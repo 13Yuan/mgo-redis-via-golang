400f5d50e7f8f37647f81476b757c067d97f373c
Ruan, Ryan <Ryan.Ruan@moodys.com>
2018-07-12 11:16:49 +0800

    11.16

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ EXPOSE 9093
 ONBUILD COPY . /go/src/MA.Content.Services.OrgMapper
 ONBUILD RUN go-install download
 ONBUILD RUN go-install install
-RUN go get -d -v
-RUN go install -v
+RUN go get -d -v ./...
+RUN go install -v ./...
--- src/domain/identifiers.go
+++ src/domain/identifiers.go
@@ -1,23 +1,7 @@
 package domain
 
-import (
-	"encoding/json"
-	"MA.Content.Services.OrgMapper/src/db"
-)
-
 type Identifiers struct {
 	SourceInfo []string `json:"Source"`
 	LabelInfo string `json:"Label"`
 	ValueInfo string `json:"Value"`
-}
-
-func (i Identifiers) Get(key string) error {
-	data, err := db.Get(key)
-	if err != nil {
-		return err
-	}
-	if err := json.Unmarshal(data, &i); err != nil {
-		return err
-	}
-	return nil
 }
